b43efc7064c0736be09c4716fe711520559b00a8
Asmaa <asmaabasher08@gmail.com>
2023-02-04 12:12:11 +0200

    Add project files.

---
+++ core/Entities/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace core.Entities
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Number { get; set; }
+    }
+}
---
+++ core/Entities/portfolioitem.cs
@@ -0,0 +1,9 @@
+﻿namespace core.Entities
+{
+    public class portfolioitem : EntityBase
+    {
+        public string ProjectName  { get; set; }
+        public string Discriptio { get; set; }
+        public string ImageUrl { get; set; } 
+    }
+}
